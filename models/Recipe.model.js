d5faab3585cb33ae2631b29b193258fcb37edbf2
LeoSantini <95887191+LeoSantini@users.noreply.github.com>
2022-02-24 15:50:33 -0300

    done

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+{
+  // TODO: Iteration 1
+  //   Create a Recipe model inside of the file /models/Recipe.model.js. The schema should have the following fields:
+  // title - Type String. It should be required and unique.
+  // level - Type String. Can be one of the following values: Easy Peasy - Amateur Chef - UltraPro Chef (remember the enum validator 😉).
+  // ingredients - Type Array of Strings (represented as [ String ]).
+  // cuisine - Type String. Should be required.
+  // dishType - Type String. Possible values: breakfast, main_course, soup, snack, drink, dessert or other.
+  // image - Type String. Default value: "https://images.media-allrecipes.com/images/75131.jpg".
+  // duration - Type Number. The minimum value should be 0.
+  // creator - Type String.
+  // created - Type Date. By default, today.
+}
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: { type: String, required: true, unique: true },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+  },
+  ingredients: [{ type: String }],
+  cuisine: { type: String, required: true },
+  dishType: {
+    type: String,
+    enum: [
+      "breakfast",
+      "main_course",
+      "soup",
+      "snack",
+      "drink",
+      "dessert",
+      "other",
+    ],
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  duration: { type: Number, min: 0 },
+  creator: { type: String },
+  created: { type: Date, default: new Date() },
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;
